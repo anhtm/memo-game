86335e1cc46a44fbc5a524013a928eec745cf128
Minh Anh Trin <minhanh.xiu@gmail.com>
2017-07-11 22:09:26 +0000

    Changes to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,66 +1,78 @@
 var cardArray = ['tiger', 'tiger', 'zebra', 'zebra', 'crocodile', 'crocodile', 'koala', 'koala',
-				'hippo', 'hippo', 'giraffe', 'giraffe', 'parrot', 'parrot', 'elephant', 'elephant'];
+  'hippo', 'hippo', 'giraffe', 'giraffe', 'parrot', 'parrot', 'elephant', 'elephant'
+];
 
 class Tile {
-	constructor() {
-	    this.hidden = true;
-	    this.content;
-	}
+  constructor() {
+    this.hidden = true;
+    this.content;
+  }
 }
 
 var tilesList = [];
 
 for (var i = 0; i < 16; i++) {
-		var key = 'tile' + (i + 1);
-    this[ key ] = new Tile;
-    this[ key ].content = cardArray[i];
-    tilesList.push(key);
+  var key = new Tile;
+  key.content = cardArray[i];
+  tilesList.push(key);
 }
 
+class Game {
+  constructor() {
+    this.board = this.getBlankBoard();
+    this.tiles = this.setTiles();
+    this.visibleTiles = [];
+    this.tilesFlipped = 0;
+  }
 
-class Board {
-    constructor() {
-        this.game = this.getBlankBoard();
-        this.tiles = this.setTiles();
-        this.match = 0;
-    }
-  	
-    getBlankBoard() {
-    	return ['-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-'];
-    }
-    
-    setTiles() {
-    	return tilesList.sort(function() {
-			return 0.5 - Math.floor(Math.random()*16);
-		});
-    }
-    
-  //TODO: Link the tiles(this.tiles) with the board(this.game)
-  
-    play(i) {
-    		var selected = this.tiles[i];
-        selected.hidden = false;
-        return selected;
-    }
+  newGame() {
+    this.board = this.getBlankBoard();
+    this.tilesFlipped = 0;
+  }
 
-    checkPair(a,b) {
-  	    var spotA = this.play(a);
-        var spotB = this.play(b);
-        if (spotA.content != spotB.content) {
-    			spotA.hidden = true;
-        	spotB.hidden = true;
-        } else if (spotA.content == spotB.content) {
-    	   	match++;
-        }
-  	}
-   
-   hasEnded() {
-   	if (match == 8) {
-    	alert("Congratulations!");
-      this.game = this.getBlankBoard();
-      this.tiles = this.setTiles();
-      this.match = 0;
+  getBlankBoard() {
+    return ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'];
+  }
+
+  setTiles() {
+    return tilesList.sort(function() {
+      return 0.5 - Math.floor(Math.random() * 16);
+    });
+  }
+
+  checkHidden(i) {
+    return this.tiles[i].hidden === true;
+  }
+
+  play(i) {
+    if (this.checkHidden(i)) {
+      this.tiles[i].hidden = false;
+      this.visibleTiles.push(this.tiles[i]);
+    } else {
+      alert("This spot is already openned!");
+      return false;
     }
-   }
-  
+  }
+
+  checkPair(a, b) {
+    if (this.play(a) && this.play(b)) {
+      if (this.tiles[a].content === this.tiles[b].content) {
+        this.tilesFlipped += 2;
+        this.visibleTiles === [];
+      } else {
+        this.tiles[a].hidden = true;
+        this.tiles[b].hidden = true;
+        this.visibleTiles === [];
+      }
+    }
+  }
+
+  hasEnded() {
+    if (this.tilesFlipped === 16) {
+      alert("Congratulations! The board will be reset.");
+      this.newGame();
+    }
+    return false;
+  }
+
 }
