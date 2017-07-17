54932176c4cda6ec42d61f940232a455ad6e3313
Minh Anh Trin <minhanh.xiu@gmail.com>
2017-07-17 15:02:43 +0000

    Finish functionalities and add comments

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,105 +1,125 @@
-var urlArray = [
-  ["squirrel", "https://image.flaticon.com/icons/png/512/26/26098.png"],
-  ["zebra", "https://d30y9cdsu7xlg0.cloudfront.net/png/115587-200.png"],
-  ["rabbit", "https://maxcdn.icons8.com/Share/icon/ios7/Holidays//easter_rabbit1600.png"],
-  ["koala", "https://d30y9cdsu7xlg0.cloudfront.net/png/176779-200.png"],
-  ["monkey", "https://www.shareicon.net/download/2016/01/17/704525_zoo_512x512.png"],
-  ["giraffe", "https://image.flaticon.com/icons/png/512/12/12606.png"],
-  ["parrot", "https://www.shareicon.net/download/2016/11/28/857811_bird_512x512.png"],
-  ["elephant", "https://d30y9cdsu7xlg0.cloudfront.net/png/14048-200.png"],
-];
+var animalArray = ["squirrel", "zebra", "rabbit", "koala", "monkey", "giraffe", "parrot", "elephant"];
+
 
 class Tile {
   constructor() {
     this.hidden = true;
     this.content;
-    this.url;
   }
 }
 
+
+// Creating an array of Tile objects
 var tilesList = [];
 
-for (var i = 0; i < urlArray.length; i++) {
+for (var i = 0; i < animalArray.length; i++) {
   var key = new Tile();
-  key.content = urlArray[i][0];
-  key.url = urlArray[i][1];
+  key.content = animalArray[i];
   var key2 = jQuery.extend(true, {}, key);
   tilesList.push(key);
   tilesList.push(key2);
 }
 
+
 class Game {
     
   constructor() {
+      
     this.tiles = this.randomizeTiles();
     this.visibleActiveTiles = [];
     this.tilesFlipped = 0;
+    
   }
 
+
+  // This method resets the game to default mode and randomize the tiles.
   newGame() {
+      
     $(".tile").remove();
-    this.tilesFlipped = 0;
-    this.visibleActiveTiles = [];
     this.tiles = this.randomizeTiles();
+    this.visibleActiveTiles = [];
+    this.tilesFlipped = 0;
     this.setHidden();
     this.drawTiles();
     registerTileClick();
+    
   }
-  
+ 
+ 
+  // This method is used in this.newGame() to set all the tiles to hidden.
   setHidden() {
+      
       for (var i = 0; i < this.tiles.length; i++) {
           this.tiles[i].hidden = true;
       }
+      
   }
 
+
   randomizeTiles() {
+      
     return tilesList.sort(function() {
       return 0.5 - Math.floor(Math.random() * 16);
     });
+    
   }
 
 
+  checkHidden(i) {
+      
+    return this.tiles[i].hidden === true;
+    
+  }
+  
+  
+  // This method handles each move and the logic behind the game.
   play(i) {
   
   	if (this.checkHidden(i) === true) {
+  	    
     	this.tiles[i].hidden = false;
         this.visibleActiveTiles.push(this.tiles[i]);
-        //$("#" + i).removeClass("hidden");
+        
       if (this.visibleActiveTiles.length === 2) {
       
       	if (this.visibleActiveTiles[0].content === this.visibleActiveTiles[1].content) {
+      	    
           this.tilesFlipped += 2;
-          //alert("You have found a pair!");
           this.visibleActiveTiles = [];
           this.hasEnded();
+          
         } else if (this.visibleActiveTiles[0].content !== this.visibleActiveTiles[1].content) {
+            
           this.visibleActiveTiles[0].hidden = true;
           this.visibleActiveTiles[1].hidden = true;
           this.visibleActiveTiles = [];
+          
         }
       }
     }
   }
    
   
-  checkHidden(i) {
-    return this.tiles[i].hidden === true;
-  }
-  
-  
   hasEnded() {
+      
     if (this.tilesFlipped === 16) {
       alert("Congratulations! You have found all pairs");
       this.newGame();
     }
+    
   }
 
+
+  // This method creates HTML divs elements representing the Tile objects.
   drawTiles() {
+      
         for (var i = 0; i < this.tiles.length; i++) {
         $("#board").append($('<div class="tile hidden ' +this.tiles[i].content + '" id=' + i + '></div>'));
-	}
+	   }
+	   
     }
-    
+
+  // This method is called after each move to redraw HTML tiles updated from JavaScript file.
   redraw() {
 
         for (var i = 0; i < this.tiles.length; i++) {
@@ -111,15 +131,21 @@ class Game {
         }
     }
     
-    timeOut(i) {
+    
+  // This method is called when an element is clicked, creating tile-appearance effect.
+  timeOut(i) {
         $("#" +i).removeClass('hidden');
     }
 }
 
+
+// Linking JS with HTML
 var ng = new Game();
 
 function registerTileClick() {
+    
     $(".tile").click(function() {
+        
       	var id = $(this).attr('id');
       	ng.timeOut(id);
 
@@ -132,6 +158,7 @@ function registerTileClick() {
 }
 
 $(document).ready(function(){
+    
     ng.drawTiles();
     registerTileClick(); 
     $("#reset").click(function() {
